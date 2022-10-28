d2cc248daa7819478e545345c1f5bf4fab956588
phuef <phil.hueffer@gmail.com>
2022-10-28 16:31:04 +0200

    init starting page

---
+++ phuef.github.io/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <StartingPage />
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import StartingPage from './components/StartingPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    StartingPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ phuef.github.io/src/components/StartingPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartingPage',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
